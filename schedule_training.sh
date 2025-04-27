734f2700cb491d94525f45f7769337681f6c640c
Vojislav S <vojislavstevanovic171@gmail.com>
2025-04-27 17:22:17 +0000

    update: schedule training with comments

--- schedule_training.sh
+++ schedule_training.sh
@@ -1,6 +1,7 @@
+# To get the models used in the final ensemble, run this script.
 python training/src/train/train_kfold.py model=shufflenetv2 datamodule.data_dir=data/kfold_train/ model.freeze_layers=false datamodule.batch_size=256 trainer.max_epochs=100
 
 python training/src/train/train_kfold.py model=mobilenetv3 datamodule.data_dir=data/kfold_train/ model.freeze_layers=false datamodule.batch_size=128 model.optimizer.lr=0.00001 trainer.max_epochs=100
 
-# for some reason, squeezenet training slows down immencely when nearing the maximum VRAM capacity, so a reduced batch size is needed for the training to go smoothly. convergence is still good
-python training/src/train/train_kfold.py model=squeezenet1_1 datamodule.data_dir=data/kfold_train/ model.freeze_layers=false datamodule.batch_size=200 trainer.max_epochs=100 model.optimizer.lr=0.00001
+# for some reason, squeezenet training slows down immencely when nearing the maximum VRAM capacity, so a reduced batch size is needed for the training to go smoothly. Convergence is still good
+# python training/src/train/train_kfold.py model=squeezenet1_1 datamodule.data_dir=data/kfold_train/ model.freeze_layers=false datamodule.batch_size=200 trainer.max_epochs=100 model.optimizer.lr=0.00001
