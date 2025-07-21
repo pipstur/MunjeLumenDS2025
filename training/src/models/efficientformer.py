b02a3bec29fb4ba1e077ef889d65f1cbbfcad593
Vojislav S <vojislav@darwindigital.com>
2025-07-21 13:38:58 +0000

    add: transformer architectures

--- training/src/models/efficientformer.py
+++ training/src/models/efficientformer.py
@@ -1,6 +1,6 @@
+import timm
 import torch
 from torch import nn
-from torchvision.models import ShuffleNet_V2_X1_0_Weights, shufflenet_v2_x1_0
 
 from training.src.models.components.loss_functions import get_loss_function
 from training.src.models.components.model_class import Model
@@ -8,21 +8,7 @@ from training.src.models.components.model_class import Model
 torch.use_deterministic_algorithms(True, warn_only=True)
 
 
-class ShuffleNetV2(Model):
-    """Implementation of LightningModule.
-
-    A LightningModule organizes your PyTorch code into 6 sections:
-        - Computations (init)
-        - Train loop (training_step)
-        - Validation loop (validation_step)
-        - Test loop (test_step)
-        - Prediction Loop (predict_step)
-        - Optimizers and LR Schedulers (configure_optimizers)
-
-    Docs:
-        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
-    """
-
+class EfficientFormer(Model):
     def __init__(
         self,
         optimizer: torch.optim.Optimizer,
@@ -34,16 +20,21 @@ class ShuffleNetV2(Model):
 
         self.criterion = get_loss_function(loss_function)
 
-        backbone = shufflenet_v2_x1_0(weights=ShuffleNet_V2_X1_0_Weights.IMAGENET1K_V1)
-        num_filters = backbone.fc.in_features
+        # Use num_classes=0 to remove the default classifier
+        backbone = timm.create_model("efficientformer_l1", pretrained=True, num_classes=0)
+        num_filters = backbone.num_features
 
-        self.feature_extractor = nn.Sequential(*list(backbone.children())[:-1])
+        self.feature_extractor = backbone
 
         if freeze_layers:
             for param in self.feature_extractor.parameters():
                 param.requires_grad = False
 
         self.classifier = nn.Linear(num_filters, self.num_classes)
+
+    def forward(self, x):
+        features = self.feature_extractor(x)
+        return self.classifier(features)
 
 
 if __name__ == "__main__":
@@ -52,5 +43,5 @@ if __name__ == "__main__":
     import pyrootutils
 
     root = pyrootutils.setup_root(__file__, pythonpath=True)
-    cfg = omegaconf.OmegaConf.load(root / "configs" / "model" / "shufflenetv2.yaml")
+    cfg = omegaconf.OmegaConf.load(root / "configs" / "model" / "efficientformer.yaml")
     _ = hydra.utils.instantiate(cfg)
